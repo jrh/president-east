48a2ddda140dac0437667dc09202cbd3dca0f234
Jeffrey Huang <jeffreyrhuang@gmail.com>
2020-04-09 20:45:45 -0600

    feat: unique index on reset_password_token in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_214101) do
+ActiveRecord::Schema.define(version: 2020_04_10_024417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2020_04_04_214101) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role"], name: "index_users_on_role"
   end
 
