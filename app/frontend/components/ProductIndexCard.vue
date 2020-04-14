0a3af9511f4db73ce8c2ac52530dea72ac96e58e
Jeffrey Huang <jeffreyrhuang@gmail.com>
2020-04-13 22:44:17 -0600

    feat: add item no info to top left of product index card

--- app/frontend/components/ProductIndexCard.vue
+++ app/frontend/components/ProductIndexCard.vue
@@ -1,13 +1,15 @@
 <template>
   <b-card footer-bg-variant="light" class="product-card-style">
+    <span class="position-absolute text-secondary" style="font-size: 11px; top: 4px; left: 6px">#{{ product.item_no }}</span>
     <span v-if="product.storage_temp !== 'Room'" class="position-absolute text-info" style="font-size: 11px; top: 4px; right: 6px">{{ product.storage_temp }}</span>
+    <b-row style="height: 10px"></b-row>
     <b-row align-h="center" align-v="center" style="height: 100px">
       <div class="thumbnail" @click="$emit('product-clicked', product.id)">
         <img v-if="product.image_url" :src="product.image_url" class="thumbnail-image" />
         <font-awesome-icon v-else :icon="['far', 'image']" size="3x" fixed-width style="color: #e6e6e6" />
       </div>
     </b-row>
-    <b-card-body class="font-lato d-flex flex-column" style="height: 130px" @click="$emit('product-clicked', product.id)">
+    <b-card-body class="font-lato d-flex flex-column" style="height: 135px" @click="$emit('product-clicked', product.id)">
       <b-container fluid>
         <b-row class="mb-1">
           <span style="font-size: 14px; color: #008000">{{ product.name_en }}</span>
